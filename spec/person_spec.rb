26c690816418fadbcd5adadca5eb2df012004fee
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-20 08:28:33 -0500

    Write person initialize test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,1 +1,19 @@
 
+require 'rspec'
+require './lib/person'
+
+describe Person do
+  describe 'initialize' do
+    it 'is an instance of person' do
+      data = {
+        last_name: "Kelly",
+        first_name: "Sue",
+        gender: "Female",
+        favortie_color: "Pink",
+        dob: "7/12/1959"
+      }
+      person = Person.new(data)
+      expect(person).to be_a(Person)
+    end
+  end
+end
