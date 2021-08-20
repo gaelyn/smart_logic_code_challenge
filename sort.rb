a87ac5e43ad86a9bfe0c36c1727c05c2d8ac99a9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-20 10:45:23 -0500

    Add option to print all outputs

--- sort.rb
+++ sort.rb
@@ -4,8 +4,10 @@ sort_gender = FileSorter.sort_gender
 sort_birthdate = FileSorter.sort_birthdate
 sort_last_name = FileSorter.sort_last_name
 
-p "Would you like the list sorted by gender, date of birth, or last name?"
+p "Would you like the list sorted by gender, date of birth, or last name? You may also type 'all' to see all sorted options."
 prompt = gets.chomp.downcase
+puts "\n"
+
 
 if prompt == "gender"
   FileSorter.print_results(sort_gender)
@@ -13,6 +15,8 @@ elsif prompt == "date of birth"
   FileSorter.print_results(sort_birthdate)
 elsif prompt == "last name"
   FileSorter.print_results(sort_last_name)
+elsif prompt == "all"
+  FileSorter.print_all_options
 else
   p "Not a valid response"
 end
