8ff3319e3dba0b0533cada3059d6ad07582d0415
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-20 08:58:18 -0500

    Sort by gender, then last name

--- lib/file_sorter.rb
+++ lib/file_sorter.rb
@@ -48,4 +48,14 @@ class FileSorter
       Person.new(data)
     end
   end
+
+  def self.all_files
+    self.load_space + self.load_comma + self.load_pipe
+  end
+
+  def self.sort_gender
+    sorted = all_files.sort_by do |person|
+      [person.gender, person.last_name]
+    end
+  end
 end
--- spec/file_sorter_spec.rb
+++ spec/file_sorter_spec.rb
@@ -21,4 +21,26 @@ describe FileSorter do
       expect(FileSorter.load_space.count).to eq(3)
     end
   end
+
+  describe 'all files' do
+    it 'can combine all loaded files into one array' do
+      expect(FileSorter.all_files).to be_an(Array)
+      expect(FileSorter.all_files.first).to be_a(Person)
+      expect(FileSorter.all_files.count).to eq(9)
+    end
+  end
+
+  describe 'sort files' do
+    before :each do
+      @files = FileSorter.all_files
+    end
+
+    it 'can sort by gender and then last name' do
+      expect(FileSorter.sort_gender).to be_an(Array)
+      expect(FileSorter.sort_gender.first.gender).to eq("Female")
+      expect(FileSorter.sort_gender.first.last_name).to eq("Hingis")
+      expect(FileSorter.sort_gender.last.gender).to eq("Male")
+      expect(FileSorter.sort_gender.last.last_name).to eq("Smith")
+    end
+  end
 end
