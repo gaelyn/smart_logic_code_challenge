fca8b482df1c5c7bc4d54509d1e366bebfa2fdaf
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-20 09:19:31 -0500

    Add other output tests

--- spec/file_sorter_spec.rb
+++ spec/file_sorter_spec.rb
@@ -85,5 +85,36 @@ describe FileSorter do
 
       expect { FileSorter.print_results(@sort_gender) }.to output(msg).to_stdout
     end
+
+    it 'can show files sorted by birthdate' do
+      msg = <<~SORTED
+      Abercrombie Neil Male 2/13/1943 Tan
+      Kelly Sue Female 7/12/1959 Pink
+      Bishop Timothy Male 4/23/1967 Yellow
+      Seles Monica Female 12/2/1973 Black
+      Bonk Radek Male 6/3/1975 Green
+      Bouillon Francis Male 6/3/1975 Blue
+      Kournikova Anna Female 6/3/1975 Red
+      Hingis Martina Female 4/2/1979 Green
+      Smith Steve Male 3/3/1985 Red
+      SORTED
+
+      expect { FileSorter.print_results(@sort_birthdate) }.to output(msg).to_stdout
+    end
+    it 'can show files sorted by gender' do
+      msg = <<~SORTED
+      Smith Steve Male 3/3/1985 Red
+      Seles Monica Female 12/2/1973 Black
+      Kournikova Anna Female 6/3/1975 Red
+      Kelly Sue Female 7/12/1959 Pink
+      Hingis Martina Female 4/2/1979 Green
+      Bouillon Francis Male 6/3/1975 Blue
+      Bonk Radek Male 6/3/1975 Green
+      Bishop Timothy Male 4/23/1967 Yellow
+      Abercrombie Neil Male 2/13/1943 Tan
+      SORTED
+
+      expect { FileSorter.print_results(@sort_last_name) }.to output(msg).to_stdout
+    end
   end
 end
