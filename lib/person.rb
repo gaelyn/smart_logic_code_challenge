461880fc6710d440b31bec5d22509d7beb3906f9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-20 08:32:51 -0500

    Added attr_reader and tested attributes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
-
+class Person
+  attr_reader :last_name, :first_name, :gender, :dob, :favorite_color
+  def initialize(data)
+    @last_name = data[:last_name]
+    @first_name = data[:first_name]
+    @gender = data[:gender]
+    @dob = data[:dob]
+    @favorite_color = data[:favorite_color]
+  end
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,16 +4,27 @@ require './lib/person'
 
 describe Person do
   describe 'initialize' do
-    it 'is an instance of person' do
+    before :each do
       data = {
         last_name: "Kelly",
         first_name: "Sue",
         gender: "Female",
-        favortie_color: "Pink",
+        favorite_color: "Pink",
         dob: "7/12/1959"
       }
-      person = Person.new(data)
-      expect(person).to be_a(Person)
+      @person = Person.new(data)
+    end
+
+    it 'is an instance of person' do
+      expect(@person).to be_a(Person)
+    end
+
+    it 'has attributes' do
+      expect(@person.last_name).to eq("Kelly")
+      expect(@person.first_name).to eq("Sue")
+      expect(@person.gender).to eq("Female")
+      expect(@person.favorite_color).to eq("Pink")
+      expect(@person.dob).to eq("7/12/1959")
     end
   end
 end
